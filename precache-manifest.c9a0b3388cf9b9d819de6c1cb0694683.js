b8b116797457593b82c54fa18aa250ce4727f4c4
WANG WEIQIANG <wangwq05@163.com>
2019-01-09 11:44:42 +0800

    Updates

---
+++ precache-manifest.c9a0b3388cf9b9d819de6c1cb0694683.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41d9dd1d21a92c4e1fc5",
+    "url": "/gobang/static/js/runtime~main.41d9dd1d.js"
+  },
+  {
+    "revision": "192228aa03c18d7e2de0",
+    "url": "/gobang/static/js/main.192228aa.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/gobang/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "192228aa03c18d7e2de0",
+    "url": "/gobang/static/css/main.77d4f8d4.chunk.css"
+  },
+  {
+    "revision": "7442411319046c53424bd7b7234a354e",
+    "url": "/gobang/index.html"
+  }
+];
